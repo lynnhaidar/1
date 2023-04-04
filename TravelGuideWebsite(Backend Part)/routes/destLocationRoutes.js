4c45266a035b1410b8e3a1a0e7e2cf5ca9c9e0dd
Lynn Haidar <122235488+lynnhaidar@users.noreply.github.com>
2023-04-04 23:16:31 +0300

    Delete TravelGuideWebsite(Backend Part) directory

--- TravelGuideWebsite(Backend Part)/routes/destLocationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const destLocationController = require("../controllers/destLocationController");
-
-router.post("/country", destLocationController.createCountry);
-router.put("/:id", destLocationController.updateDestination);
-
-module.exports = router;
--- TravelGuideWebsite(Backend Part)/routes/weatherRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const weatherController = require("../controllers/weatherController");
-
-router.get("/weather/:country", weatherController.getWeatherByCountry);
-router.get("/", weatherController.getWeathersOfAllCountries);
-
-module.exports = router;
